387ac09b6e04113eba5cda86b8ab6890ae237332
Chris Ohk <utilforever@gmail.com>
2017-07-18 12:59:24 +0900

    Upload Vector.h - Initial commit

---
+++ Includes/Vector.h
@@ -0,0 +1,37 @@
+/*************************************************************************
+> File Name: Vector.h
+> Project Name: CubbySTL
+> Author: Chan-Ho Chris Ohk
+> Purpose: Implements a vector, much like the C++ std::vector class.
+> Created Time: 2017/07/18
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
+#ifndef CUBBYSTL_VECTOR_H
+#define CUBBYSTL_VECTOR_H
+
+#include <type_traits>
+
+namespace CubbySTL
+{
+	template <typename T, typename Alloc = Allocator<T>>
+	class Vector
+	{
+	public:
+		typedef T			value_type;
+		typedef T*			iterator;
+		typedef const T&	const_iterator;
+		typedef T*			pointer;
+		typedef const T*	const_pointer;
+		typedef T&			reference;
+		typedef const T&	const_reference;
+		typedef size_t		size_type;
+		typedef ptrdiff_t	difference_type;
+
+	private:
+		T*		m_begin;
+		T*		m_end;
+		Alloc	m_allocator;
+	};
+}
+
+#endif
